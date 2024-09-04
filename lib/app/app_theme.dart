736eadb687e3f3bbd1b2efdcfff0a16e8baafa26
dherediat <dherediat97@gmail.com>
2024-09-04 23:47:20 +0200

    feature: add animation, fix sizes of basic container

---
+++ lib/app/app_theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const defaultGradient = LinearGradient(
+    colors: [Colors.pink, Colors.purple],
+  );
+}
--- lib/model/portfolio_model.dart
+++ lib/model/portfolio_model.dart
@@ -1,29 +1,3 @@
-import 'dart:collection';
-
 import 'package:flutter/material.dart';
 
-class PortfolioModel extends ChangeNotifier {
-  /// Internal, private state of the cart.
-  final List<String> _items = [];
-
-  /// An unmodifiable view of the items in the cart.
-  UnmodifiableListView<String> get items => UnmodifiableListView(_items);
-
-  /// The current total price of all items (assuming all items cost $42).
-  int get totalPrice => _items.length * 42;
-
-  /// Adds [item] to cart. This and [removeAll] are the only ways to modify the
-  /// cart from the outside.
-  void add(String item) {
-    _items.add(item);
-    // This call tells the widgets that are listening to this model to rebuild.
-    notifyListeners();
-  }
-
-  /// Removes all items from the cart.
-  void removeAll() {
-    _items.clear();
-    // This call tells the widgets that are listening to this model to rebuild.
-    notifyListeners();
-  }
-}
+class PortfolioModel extends ChangeNotifier {}
