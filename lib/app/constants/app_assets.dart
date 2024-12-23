e32f78074a4a221ee3984ebdb9022bc871434a5a
dherediat <dherediat97@gmail.com>
2024-12-23 12:32:32 +0100

    feature: delete all, migrate idea to astro

--- lib/app/constants/app_assets.dart
+++
@@ -1,7 +0,0 @@
-import 'package:portfolio/app/config/enviroment_config.dart';
-
-class AppAssets {
-  static const String assetsFolder =
-      EnvironmentConfig.environment == 'prod' ? 'assets' : '';
-  static const String imagesFolder = '$assetsFolder/images';
-}
